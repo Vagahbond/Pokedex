096fbe701c0965609947c47db56514ab81903e92
Yoni <firroloni.yoni@gmail.com>
2020-11-23 01:18:43 +0100

    add main activity

--- app/src/main/java/com/dedistonks/pokedex/MainActivity.kt
+++ app/src/main/java/com/dedistonks/pokedex/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.dedistonks.pokedex
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import com.dedistonks.pokedex.objects.ObjectListActivity
+import com.dedistonks.pokedex.pokemons.PokemonListActivity
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navToPokemonsButton : Button = findViewById(R.id.btPokemons)
+        val navToObjectsButton : Button = findViewById(R.id.btObjects)
+
+        navToPokemonsButton.setOnClickListener {
+            val intent = Intent(this, PokemonListActivity::class.java)
+            startActivity(intent)
+        }
+
+        navToObjectsButton.setOnClickListener{
+            val intent = Intent(this, ObjectListActivity::class.java)
+            startActivity(intent)
+        }
     }
+
 }
