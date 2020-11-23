cdf5185e681a2b715bdb2862c81b74ae14395ecd
quantumsheep <nathanael.dmc@outlook.fr>
2020-11-24 00:20:11 +0100

    Bind getPokemon into a DTO

--- app/src/main/java/com/dedistonks/pokedex/api/PokeAPI.kt
+++ app/src/main/java/com/dedistonks/pokedex/api/PokeAPI.kt
@@ -29,6 +29,27 @@ class PokeAPI {
         }
     }
 
+    data class PokemonSpritesDTO(
+        val back: String?,
+        val front: String?,
+        val backShiny: String?,
+        val frontShiny: String?,
+    )
+
+    data class PokemonEvolutionDTO(val name: String, val sprites: PokemonSpritesDTO)
+
+    data class PokemonDTO(
+        val id: Int,
+        val name: String,
+        val description: String?,
+        val height: Int,
+        val abilities: List<String>,
+        val types: List<String>,
+        val sprites: PokemonSpritesDTO,
+        val evolutions: List<PokemonEvolutionDTO>,
+        val games: List<String>,
+    )
+
     fun getPokemons(offset: Int = 0, limit: Int = 5, callback: (List<NamedApiResource>) -> Unit) {
         thread {
             val items = api.getPokemonList(offset, limit)
@@ -36,10 +57,42 @@ class PokeAPI {
         }
     }
 
-    fun getPokemon(id: Int, callback: (Pokemon) -> Unit) {
+    fun getPokemon(id: Int, callback: (PokemonDTO) -> Unit) {
         thread {
             val pokemon = api.getPokemon(id)
-            callback(pokemon)
+            val pokedex = api.getPokedex(id)
+            val evolutions = api.getEvolutionChain(id)
+
+            val dto = PokemonDTO(
+                id = pokemon.id,
+                name = pokemon.name,
+                description = pokedex.descriptions.find { description -> description.language.name == "en" }!!.description,
+                height = pokemon.height,
+                abilities = pokemon.abilities.map { ability -> ability.ability.name },
+                types = pokemon.types.map { type -> type.type.name },
+                sprites = PokemonSpritesDTO(
+                    back = pokemon.sprites.backDefault,
+                    front = pokemon.sprites.frontDefault,
+                    backShiny = pokemon.sprites.backShiny,
+                    frontShiny = pokemon.sprites.frontShiny,
+                ),
+                evolutions = evolutions.chain.evolvesTo.map { to ->
+                    val pokemon = api.getPokemon(to.species.id)
+
+                    PokemonEvolutionDTO(
+                        name = pokemon.name,
+                        sprites = PokemonSpritesDTO(
+                            back = pokemon.sprites.backDefault,
+                            front = pokemon.sprites.frontDefault,
+                            backShiny = pokemon.sprites.backShiny,
+                            frontShiny = pokemon.sprites.frontShiny,
+                        ),
+                    )
+                },
+                games = pokemon.gameIndices.map { game -> game.version.name },
+            )
+
+            callback(dto)
         }
     }
 }
