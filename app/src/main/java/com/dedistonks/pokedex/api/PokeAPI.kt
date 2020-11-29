392b915234ddeb24b6ab38329d133c36f864e17e
quantumsheep <nathanael.dmc@outlook.fr>
2020-11-29 13:13:54 +0100

    Externalize thread function

--- app/src/main/java/com/dedistonks/pokedex/api/PokeAPI.kt
+++ app/src/main/java/com/dedistonks/pokedex/api/PokeAPI.kt
@@ -1,29 +1,22 @@
 package com.dedistonks.pokedex.api
 
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
+import com.dedistonks.pokedex.utils.Thread
 import me.sargunvohra.lib.pokekotlin.client.PokeApiClient
 import me.sargunvohra.lib.pokekotlin.model.Item
 import me.sargunvohra.lib.pokekotlin.model.NamedApiResource
-import me.sargunvohra.lib.pokekotlin.model.Pokemon
 
 class PokeAPI {
     private val api = PokeApiClient()
 
-    private fun thread(callback: () -> Unit) {
-        GlobalScope.launch(Dispatchers.IO) { callback() }
-    }
-
     fun getItems(offset: Int = 0, limit: Int = 5, callback: (List<NamedApiResource>) -> Unit) {
-        thread {
+        Thread.thread {
             val items = api.getItemList(offset, limit)
             callback(items.results)
         }
     }
 
     fun getItem(id: Int, callback: (Item) -> Unit) {
-        thread {
+        Thread.thread {
             val item = api.getItem(id)
             callback(item)
         }
@@ -51,14 +44,14 @@ class PokeAPI {
     )
 
     fun getPokemons(offset: Int = 0, limit: Int = 5, callback: (List<NamedApiResource>) -> Unit) {
-        thread {
+        Thread.thread {
             val items = api.getPokemonList(offset, limit)
             callback(items.results)
         }
     }
 
     fun getPokemon(id: Int, callback: (PokemonDTO) -> Unit) {
-        thread {
+        Thread.thread {
             val pokemon = api.getPokemon(id)
             val pokedex = api.getPokedex(id)
             val evolutions = api.getEvolutionChain(id)
