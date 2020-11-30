93d09c4fe4e841f5aa1c77f3859d0cecf0fe74ab
Yoni <firroloni.yoni@gmail.com>
2020-11-30 02:59:59 +0100

    add pokemon pictures

--- app/src/main/java/com/dedistonks/pokedex/pokemons/PokemonDetailFragment.kt
+++ app/src/main/java/com/dedistonks/pokedex/pokemons/PokemonDetailFragment.kt
@@ -1,10 +1,9 @@
 package com.dedistonks.pokedex.pokemons
 
-import android.net.Uri
+import android.annotation.SuppressLint
+import android.graphics.Bitmap
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
-import com.google.android.material.appbar.CollapsingToolbarLayout
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,6 +11,7 @@ import android.widget.ImageView
 import android.widget.TextView
 import com.dedistonks.pokedex.R
 import com.dedistonks.pokedex.api.PokeAPI
+import com.dedistonks.pokedex.utils.ImageToBitmap
 
 /**
  * A fragment representing a single Pokemon detail screen.
@@ -46,24 +46,42 @@ class PokemonDetailFragment : Fragment() {
         val rootView = inflater.inflate(R.layout.pokemon_detail, container, false)
 
         api.getPokemon(this.pokemonID){ pokemon ->
-            activity.let {
-                it?.runOnUiThread {
-                    rootView.findViewById<ImageView>(R.id.ivPokemonFront).setImageURI(Uri.parse(pokemon.sprites.front))
-                    rootView.findViewById<ImageView>(R.id.ivPokemonBack).setImageURI(Uri.parse(pokemon.sprites.back))
-                    rootView.findViewById<ImageView>(R.id.ivPokemonShinyFront).setImageURI(Uri.parse(pokemon.sprites.frontShiny))
-                    rootView.findViewById<ImageView>(R.id.ivPokemonShinyBack).setImageURI(Uri.parse(pokemon.sprites.backShiny))
 
-                    rootView.findViewById<TextView>(R.id.twName).text = pokemon.name
+            val frontView: ImageView = rootView.findViewById(R.id.ivPokemonFront)
+            val backView: ImageView = rootView.findViewById(R.id.ivPokemonBack)
+            val sFrontView: ImageView = rootView.findViewById(R.id.ivPokemonShinyFront)
+            val sBackView: ImageView = rootView.findViewById(R.id.ivPokemonShinyBack)
+
+            pokemon.sprites.front?.let { ImageToBitmap.from(it) { bitmap -> setImageToComponent(frontView, bitmap) } }
+            pokemon.sprites.back?.let { ImageToBitmap.from(it) { bitmap -> setImageToComponent(backView, bitmap) } }
+            pokemon.sprites.frontShiny?.let { ImageToBitmap.from(it) { bitmap -> setImageToComponent(sFrontView, bitmap) } }
+            pokemon.sprites.backShiny?.let { ImageToBitmap.from(it) { bitmap -> setImageToComponent(sBackView, bitmap) } }
+
+            activity?.let {
+                it.runOnUiThread {
+                    rootView.findViewById<TextView>(R.id.twName).text = pokemon.name.capitalize()
                     rootView.findViewById<TextView>(R.id.twID).text = pokemon.id.toString()
 
                     rootView.findViewById<TextView>(R.id.tvDetails).text = pokemon.description
-                    rootView.findViewById<TextView>(R.id.tvType1).text = pokemon.types[0]
-                    rootView.findViewById<TextView>(R.id.tvType1).text =  if (pokemon.types.size > 1)  pokemon.types[1] else  ""
+                    rootView.findViewById<TextView>(R.id.tvHeight).text = getString(R.string.height_property,  String.format("%.2f",pokemon.height.toDouble().div(10))) // Yeah, for realm the API gives it in decimeters...
+
+                    rootView.findViewById<TextView>(R.id.tvType1).text = pokemon.types[0].toUpperCase()
+                    rootView.findViewById<TextView>(R.id.tvType2).text =  if (pokemon.types.size > 1)  pokemon.types[1].toUpperCase() else  ""
+
                 }
             }
         }
         return rootView
     }
+
+    private fun setImageToComponent(target: ImageView, value: Bitmap) {
+        activity?.let {
+            it.runOnUiThread{
+                target.setImageBitmap(value)
+            }
+        }
+    }
+
 
     companion object {
         /**
